9ae743f543cf3eb9ad10091d279f1ebebe6def61
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-06 10:40:32 +0200

    added navBarSimple.js + edit App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,19 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+// import Sidebar from './components/Sidebar';
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 // import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import NavBarSimple from './components/NavBarSimple';
 
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      {/* <Sidebar /> */}
       {/* <StatefulGreeting greeting = "I'm stateful class component" name = "Alakel"/> */}
       {/* <StatefulGreetingWithCallback /> */}
       {/* <StatefulGreetingWithPrevState /> */}
+      <NavBarSimple />
 
 
     </div>
