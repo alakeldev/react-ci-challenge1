42e9e442fd52e5eaca7eb47b72b8bd9185e368b4
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-09 19:42:35 +0200

    edit files js

--- my-app/src/components/NavBarSimple.js
+++ my-app/src/components/NavBarSimple.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import css from "./css/NavBarSimple.module.css"
+import React, { Component } from "react";
+import css from "./css/NavBarSimple.module.css";
 
 class NavBarSimple extends Component {
 
@@ -8,16 +8,16 @@ class NavBarSimple extends Component {
 
         this.state = {
             message: "Hello, guest!",
-            buttonText: "Log in",
+            buttonText: "Log in"
         }
     }
 
     handleclick = () => {
         this.setState((prevState) => ({
             message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
-            buttonText: prevState.buttonText === "Log out" ? "Log in" : "Log out"
-        }), () => console.log(this.state.message)
-    )
+            buttonText: prevState.buttonText === "Log out" ? "Log in" : "Log out",
+        }), () => console.log(this.state.message))
+
     }
 
     render() {
--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,15 +1,14 @@
-import React from "react";
-import css from "./css/Sidebar.module.css"
+import React from 'react';
+import css from "./css/Sidebar.module.css";
 
-
-const Sidebar = () => {
+function Sidebar() {
     return (
         <div className={css.sidebar}>
-            <a href="#" target="_blank">My Photos</a>
-            <a href="#" target="_blank">My Illustrations</a>
-            <a href="#" target="_blank">My Paintings</a>
+            <a href="#." target="_blank">My Photos</a>
+            <a href="#." target="_blank">My Illustrations</a>
+            <a href="#." target="_blank">My Paintings</a>
         </div>
     )
-};
+}
 
-export default Sidebar;
+export default Sidebar
